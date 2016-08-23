5463a52c63462d59d888916de4b3df26ae993098
Emil Holmström <emil@update.uu.se>
2016-08-24 02:09:56 +0700

    change subtitles (ugly hack)

---
+++ emms-player-vlc.el
@@ -0,0 +1,155 @@
+;;; emms-player-vlc.el --- vlc support for EMMS
+
+;; Copyright (C) 2008, 2009 Free Software Foundation, Inc.
+
+;; Authors: Yoni Rabkin <yonirabkin@member.fsf.org>
+
+;; This file is part of EMMS.
+
+;; EMMS is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+
+;; EMMS is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with EMMS; if not, write to the Free Software Foundation,
+;; Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'emms-compat)
+(require 'emms-player-simple)
+
+;; I use this macro, and later override some of the stuff it defines
+;; in order to accomodate VLC's particular idioms.
+(define-emms-simple-player vlc '(file url)
+  (concat "\\`\\(http[s]?\\|mms\\)://\\|"
+	  (apply #'emms-player-simple-regexp
+		 emms-player-base-format-list))
+  "vlc" "--control=rc" "--fullscreen")
+
+(define-emms-simple-player vlc-playlist '(streamlist)
+  "\\`http[s]?://"
+  "vlc" "--control=rc" "--fullscreen")
+
+;; (kludge) By default, VLC does not quit after finishing to play a
+;; track, so the player sentinel has no way of telling that the next
+;; track should be played. Therefore I redefine this low-level
+;; function and add a "quit" track which is invisible to Emms.
+(defadvice emms-player-vlc-start (around quit-vlc-after-finish activate)
+  (let ((process (apply 'start-process
+                        emms-player-simple-process-name
+                        emms-player-simple-process-name
+                        emms-player-vlc-command-name
+                        ;; splice in params here
+                        (append emms-player-vlc-parameters
+                                (list (emms-track-name (ad-get-arg 0)))
+                                '("vlc://quit")))))
+    ;; add a sentinel for signaling termination
+    (set-process-sentinel process 'emms-player-simple-sentinel))
+  (emms-player-started emms-player-vlc))
+
+(defun emms-player-vlc-pause ()
+  "Depends on vlc's rc mode."
+  (process-send-string
+   emms-player-simple-process-name "pause\n"))
+
+;; (defun emms-player-vlc-seek (sec)
+;;   "Seek relative within a stream."
+;;   (when (not (= 0 sec))
+;;     (process-send-string
+;;      emms-player-simple-process-name
+;;      (if (< 0 sec) "fastforward\n" "rewind\n"))))
+
+(defun emms-player-vlc-seek (sec)
+  "Seek relative within a stream."
+  (process-send-string
+    emms-player-simple-process-name
+   (format "seek %s%d\n" (if (< 0 sec) "+" "") sec)))
+
+(defun emms-player-vlc-seek-to (sec)
+  "Seek to time SEC within the stream."
+  (process-send-string
+    emms-player-simple-process-name
+   (format "seek %d\n" sec)))
+
+(emms-player-set emms-player-vlc 'pause 'emms-player-vlc-pause)
+(emms-player-set emms-player-vlc 'resume nil) ; pause is also resume
+(emms-player-set emms-player-vlc 'start 'emms-player-vlc-start)
+(emms-player-set emms-player-vlc 'seek 'emms-player-vlc-seek)
+
+(emms-player-set emms-player-vlc 'seek-to 'emms-player-vlc-seek-to)
+(emms-player-set emms-player-vlc 'get-time 'emms-player-vlc-get-time)
+(emms-player-set emms-player-vlc 'get-sub 'emms-player-vlc-get-subtitle)
+(emms-player-set emms-player-vlc 'prev-sub 'emms-player-vlc-prev-subtitle)
+(emms-player-set emms-player-vlc 'next-sub 'emms-player-vlc-next-subtitle)
+
+(defun emms-player-vlc-current-sub (lines)
+  (mapcar (lambda (x)
+            (if (string-match "\\([0-9][0-9]*\\) - \\(.*\\) \\*$" x)
+                (let ((idx (string-to-number (match-string 1 x)))
+                      (title (match-string 2 x)))
+                  (list idx (- (length lines) 3) title))))
+          lines))
+
+(defun emms-player-vlc-get-subtitle ()
+  (let* ((proc emms-player-simple-process-name)
+          (oldp (with-current-buffer proc (point))))
+    (process-send-string proc "strack\n")
+    (accept-process-output (get-process proc) 0 100)
+    (let* ((input (with-current-buffer proc (buffer-substring oldp (point))))
+           (lines (split-string input "\r\n")))
+      (car (remove-if #'null (emms-player-vlc-current-sub lines))))))
+
+(defun emms-player-vlc-prev-subtitle ()
+  (interactive)
+  (message (emms-player-vlc-change-subtitle -1)))
+
+(defun emms-player-vlc-next-subtitle ()
+  (interactive)
+  (message (emms-player-vlc-change-subtitle 1)))
+    
+(defun emms-player-vlc-change-subtitle (n)
+  (let* ((proc emms-player-simple-process-name)
+         (sub (emms-player-vlc-get-subtitle)))
+    (cond ((null sub) "none")
+          (t (let* ((idx (car sub))
+                    (tot (cadr sub))
+                    (next (+ (mod idx tot) n)))
+               (process-send-string proc (format "strack %s\n" next))
+               (caddr (emms-player-vlc-get-subtitle)))))))
+
+;; (let* ((input "+----[ spu-es ]
+;; | -1 - Disable
+;; | 2 - Track 1 *
+;; +----[ end of spu-es ]")
+;;        (lines (split-string input "\n"))
+;;        )
+
+
+  
+
+  
+
+;; > strack
+;; +----[ spu-es ]
+;; | -1 - Disable
+;; | 2 - Track 1 *
+;; +----[ end of spu-es ]
+;; > strack 1
+;; > strack
+;; +----[ spu-es ]
+;; | -1 - Disable *
+;; | 2 - Track 1
+;; +----[ end of spu-es ]
+
+(provide 'emms-player-vlc)
+
+;;; emms-player-vlc.el ends here
